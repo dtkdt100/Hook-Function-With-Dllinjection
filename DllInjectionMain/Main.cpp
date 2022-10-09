88580da09eef60efcd9ab3a7d2adeaaf1e7d86bc
Dolev Franco <dolevfranco2005@gmail.com>
2022-10-09 18:15:52 +0300

    DllInjection

---
+++ DllInjectionMain/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "DllInjection/Sample.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
---
+++ DllInjectionTests/pch.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "../packages/Microsoft.googletest.v140.windesktop.msvcstl.static.rt-dyn.1.8.1.5/build/native/include/gtest/gtest.h"
